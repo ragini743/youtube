068ec4457757736b7bf8c9409788f2fb8ff6ed56
Ragini <raginiv743@gmail.com>
2023-10-17 17:37:48 +0530

    add width and bottom margin in skelton

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,97 +1,97 @@
 const Shimmer = () =>{
     return(
         <div className="flex flex-wrap flex-col md:grid md:grid-cols-3 md:gap-8 h-full">
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
            <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 w-40 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 mt-1 w-28 rounded-lg"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
            <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56">
 
             </div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
-           <div className=" border-l-gray-400">
+           <div className=" border-l-gray-400 mb-5 md:mb-0">
             <div className="bg-slate-300 h-56"></div>
             <ul className="mt-1">
-            <li className="bg-slate-300 h-6 rounded-lg"></li>
-            <li className="bg-slate-300 h-6 mt-1 rounded-lg"></li>
+            <li className="bg-slate-300 h-6 rounded-lg w-40"></li>
+            <li className="bg-slate-300 h-6 mt-1 rounded-lg w-28"></li>
             </ul>
            </div>
 
